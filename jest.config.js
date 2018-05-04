0030d001133cd4dabdf7aca5184e3c9d624b5809
Julian Coleman <jcoleman.engineering@gmail.com>
2018-05-03 23:23:59 -0700

    test(config): add initial test configuration to project (#5)

---
+++ jest.config.js
@@ -0,0 +1,21 @@
+module.exports = {
+  collectCoverageFrom: [
+    "src/**/!(index).{js,ts}",
+    "helpers/**/!(index).{js,ts}"
+  ],
+  coverageDirectory: "coverage",
+  testMatch: [
+    "<rootDir>/src/**/?(*.)(spec|test).(js|ts)?(x)",
+    "<rootDir>/helpers/**/?(*.)(spec|test).(js|ts)?(x)",
+  ],
+  reporters: process.env.CI ? undefined : ["jest-spec-reporter"],
+  rootDir: ".",
+  moduleFileExtensions: [
+    "ts",
+    "js",
+  ],
+  transform: {
+    "^.+\\.tsx?$": "ts-jest",
+  },
+  testEnvironment: "node",
+};
